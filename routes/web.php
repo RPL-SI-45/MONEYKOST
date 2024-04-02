330e3f520a560a0567cb153f8fd4537d331a5e21
KefasPakpahan <kefas.juni@gmail.com>
2024-04-02 21:07:02 +0700

    K1RPL-3
    Menambahkan Fitur Pembayaran Wifi yang kemarin kurang

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+//wifi
+Route::get('/wifi', function () {
+    return view('pages.pembayaranwifi-customer');
 });
