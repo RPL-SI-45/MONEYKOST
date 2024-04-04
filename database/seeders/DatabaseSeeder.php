12bdd287c0e46e292e38cea4a32f6f108b0b597e
tio432 <fathinuriazhar@student.telkomuniversity.ac.id>
2024-04-04 16:30:50 +0700

    K1RPL-4
    mengerjakan css, route, controller, models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,34 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('payment_wifi')->insert([
+            'id_customer' => 1,
+            'tanggal_tagihan' => '2024-03-22',
+            'paket' => '10 Mbps',
+            'jumlah' => '75.000',
+            'bukti' => 'bruce-mars.jpg',
+            'status' => 'lunas',
+        ]);
+        DB::table('payment_wifi')->insert([
+            'id_customer' => 1,
+            'tanggal_tagihan' => '2024-03-27',
+            'paket' => '20 Mbps',
+            'jumlah' => '125.000',
+            'bukti' => '',
+            'status' => 'belum lunas',
+        ]);
     }
 }
