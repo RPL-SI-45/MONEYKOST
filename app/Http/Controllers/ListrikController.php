bfcdabaa22698765abc87266d654f3b517d40e32
eunikeodelyn <eunikeodelyn19@gmail.com>
2024-04-08 11:26:06 +0700

    K1RPL-5 & K1RPL-6 final fitur pembayaran listrik dan fitur kelola pembayaran listrik

--- app/Http/Controllers/ListrikController.php
+++ app/Http/Controllers/ListrikController.php
@@ -2,14 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\pembayaranlistrik;
 use Illuminate\Http\Request;
-use PHPUnit\TextUI\Configuration\Php;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Pembayaranlistrik;
+use Illuminate\Support\Facades\DB;
 
 class ListrikController extends Controller
 {
-    public function index(){
-        $pembayaranlistrik = pembayaranlistrik::all();
-        return view('pembayaranlistrik.index', compact(['pembayaranlistrik']));
+    public function index() {
+        $users = User::where('auth', 'customer')->get();
+        return view('pages.tambahlistrik', [
+            'users' => $users
+        ]);
+    }
+
+    public function tambah() {
+        $attributes = request()->validate([
+            'id_customer' => 'required',
+            'tanggaltagihan' => 'required',
+            'jumlah' => 'required',
+            'kwh' => 'required'
+        ]);
+        $attributes['jumlah'] = number_format($attributes['jumlah'], 0, ',', '.');
+        $attributes['bukti'] = '';
+        $attributes['status'] = 'belum lunas';
+        Pembayaranlistrik::create($attributes);
+        return redirect("/dashboard/admin/pembayaranlistrik");
+    }
+
+    public function upload(int $id) {
+        request()->validate([
+            'upload-bukti' => 'required|image|mimes:jpeg,jpg,png,gif',
+        ]);
+        if (request()->file()) {
+            $fileName = time().'_cust.'.request()->file('upload-bukti')->extension();
+            $filePath = request()->file('upload-bukti')->storeAs('bukti', $fileName, 'public');
+            $data = Pembayaranlistrik::where('id', $id)->update([
+                "bukti" => "bukti/" . $fileName,
+                "status" => "lunas"
+            ]);
+            return redirect("/dashboard/customer");
+        }
     }
 }
--- app/Models/pembayaranlistrik.php
+++ app/Models/pembayaranlistrik.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class pembayaranlistrik extends Model
 {
-    use HasFactory;
+    use HasApiTokens, Notifiable;
+
     protected $table = 'pembayaran_listrik';
+    public $timestamps = false; 
+    protected $fillable = [
+        'id_customer',
+        'tanggaltagihan',
+        'jumlah',
+        'kwh',
+        'bukti',
+        'status'
+    ];
 }
