378dc3eb19a56d528a4ea60c6487d9bb48c53526
porozera <aufarnugratamaps@gmail.com>
2024-04-17 05:41:53 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,33 +16,36 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        // DB::table('users')->insert([
-        //     'username' => 'ujang',
-        //     'email' => 'ujang@gmail.com',
-        //     'password' => bcrypt('123'),
-        //     'auth' => 'customer',
+        DB::table('users')->insert([
+            'username' => 'ujang',
+            'email' => 'ujang@gmail.com',
+            'password' => bcrypt('123'),
+            'auth' => 'customer',
+        ]);
 
-        // DB::table('payment_wifi')->insert([
-        //     'id_customer' => 1,
-        //     'tanggal_tagihan' => '2024-03-22',
-        //     'paket' => '10 Mbps',
-        // ]);
+        DB::table('payment_wifi')->insert([
+            'id_customer' => 1,
+            'tanggal_tagihan' => '2024-03-22',
+            'paket' => '10 Mbps',
+            'jumlah' => '120.000',
+            'status' => 'belum lunas',
+        ]);
 
-        // DB::table('pembayaran')->insert([
-        //     'id_customer' => 1,
-        //     'tanggal_tagihan' => '2024-03-22',
-        //     'berat' => '10 KG',
-        //     'jumlah' => '75.000',
-        //     'bukti' => 'bruce-mars.jpg',
-        //     'status' => 'lunas',
-        // ]);
+        DB::table('pembayaran_kost')->insert([
+            'id_customer' => 1,
+            'tanggal_tagihan' => '2024-03-22',
+            'tipe_kamar' => 'AC',
+            'jumlah' => '12.000.000',
+            'status' => 'belum lunas',
+        ]);
 
-        // DB::table('payment_wifi')->insert([
-        //     'id_customer' => 1,
-        //     'tanggal_tagihan' => '2024-03-27',
-        //     'paket' => '20 Mbps',
-        // ]);
-
+        DB::table('pembayaran_kost')->insert([
+            'id_customer' => 1,
+            'tanggaltagihan' => '2024-03-22',
+            'kwh' => '80',
+            'jumlah' => '90.000',
+            'status' => 'belum lunas',
+        ]);
         DB::table('pembayaran')->insert([
             'id_customer' => 1,
             'tanggal_tagihan' => '2024-03-27',
