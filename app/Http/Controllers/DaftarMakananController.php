ce71c6145c5b52e3a7792729ada0238cde1c5351
porozera <aufarnugratamaps@gmail.com>
2024-05-12 16:24:49 +0700

    K1RPL-8 Menampilkan data di kelola daftar makanan

--- app/Http/Controllers/DaftarMakananController.php
+++ app/Http/Controllers/DaftarMakananController.php
@@ -21,4 +21,37 @@ class DaftarMakananController extends Controller
         $daftar_makanan = DaftarMakanan::findOrFail($id);
         return view('pages.daftar-makanan-show', ['daftar_makanan' => $daftar_makanan]);
     }
+
+    public function adminindex() {
+        $daftar_makanan = DaftarMakanan::all();
+        return view('pages.daftar-makanan-admin', ['daftar_makanan' => $daftar_makanan ]);
+    }
+
+    public function create() {
+        $daftar_makanan = DaftarMakanan::all();
+        return view('pages.daftar-makanan-create', ['daftar_makanan' => $daftar_makanan ]);
+    }
+
+    public function tambah() {
+        $attributes = request()->validate([
+            'nama_makanan' => 'required',
+            'harga_makanan' => 'required',
+            'tipe_makanan' => 'required',
+        ]);
+        $attributes['harga_makanan'] = number_format($attributes['harga_makanan'], 0, ',', '.');
+        $attributes['gambar_makanan'] = '';
+        $attributes['deskripsi_makanan'] = '';
+        DaftarMakanan::create($attributes);
+        return redirect("/dashboard/admin/kelolamenumakanan");
+    }
+
+    public function upload(Request $request, $id) {
+        $requestData= $request->all();
+        $fileName = $file->getClientOriginalName(); // Mendapatkan nama file asli
+        $filePath = $file->storeAs('public', $fileName); // Menyimpan file dengan nama asli
+        $data = DaftarMakanan::where('id', $id)->update([
+                "gambar_makanan" => $fileName,
+            ]);
+    }
 }
+
---
+++ resources/views/pages/daftar-makanan-create.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
+@section('content')
+    @include('layouts.navbars.auth.topnav', ['title' => 'Tambah Menu Makanan', 'titleSub' => 'Admin'])
+    <div class="container-fluid py-4">
+        <div class="row">
+            <div class="col-12">
+                <div class="card mb-4">
+                    <div class="card-header pb-0 d-flex align-items-center justify-content-between">
+                        <h6>Tambah Menu Makanan</h6>
+                    </div>
+                    <div class="card-body">
+                        <form role="form" method="POST" action="{{ route('tambah-menu.perform') }}" enctype="multipart/form-data">
+                            @csrf
+                            @method('post')
+                            <div class="form-group">
+                                <label for="namaMakananInput" class="form-control-label">Nama Makanan</label>
+                                <input class="form-control" type="text" id="namaMakananInput" name="nama_makanan">
+                                @error('nama_makanan') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="hargaMakananInput" class="form-control-label">Harga Makanan</label>
+                                <input class="form-control" type="number" id="hargaMakananInput" name="harga_makanan">
+                                @error('harga_makanan') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="tipeMakananInput" class="form-control-label">Tipe</label>
+                                <select class="form-control" name="tipe_makanan">
+                                    <option selected></option>
+                                    <option value="Makanan">Makanan</option>
+                                    <option value="Minuman">Minuman</option>
+                                </select>
+                                @error('tipe_makanan') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="deskripsiMakananInput" class="form-control-label">Deskripsi Makanan</label>
+                                <textarea class="form-control" type="text" id="deskripsiMakananInput" name="deskripsi_makanan"></textarea>
+                                @error('deskripsi_makanan') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="gambarMakananInput" class="form-control-label">Gambar</label>
+                                <input class="form-control" type="file" id="gambarMakananInput" name="gambar_makanan">
+                                @error('gambar_makanan') <p class="text-danger text-xs pt-1"> {{$message}} </p>@enderror
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-lg btn-info btn-lg w-100 mt-4 mb-0">Tambah</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
